050be6eeddecd957049d898ef1f1b06a844cefd9
raihan <raihanmuhd97@gmail.com>
2022-02-14 15:02:46 +0800

    using DiceMarket, not DiceBattle

--- lab4/migrations/2_deploy_contracts.js
+++ lab4/migrations/2_deploy_contracts.js
@@ -3,9 +3,9 @@ const Dice = artifacts.require("Dice");
 const DiceMarket = artifacts.require("DiceMarket");
 
 // then use deployer to deploy dice first
-// then deploy DiceBattle by passing Dice's address to DiceBattle's constructor.
+// then deploy DiceMarket by passing Dice's address to DiceMarket's constructor.
 module.exports = (deployer, network, accounts) => {
     deployer.deploy(Dice).then(function() {
-        return deployer.deploy(DiceBattle, Dice.address);
+        return deployer.deploy(DiceMarket, Dice.address);
     });
 };
